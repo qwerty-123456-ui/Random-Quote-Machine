4f1a718af662c77e6836070a6a64a853bfd912e6
qwerty935 <isha26701@gmail.com>
2020-07-09 12:01:42 +0530

    create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: "#app",
+    data: {
+      quotes,
+      currentQuote: quotes[0],
+      animating: false,
+    },
+    methods: {
+      newQuote: function () {
+        const num = Math.floor(Math.random() * quotes.length)
+        this.currentQuote = quotes[num]
+        this.animating = true
+        setTimeout(() => {
+          this.animating = false
+        }, 1000)
+      },
+    },
+  })
